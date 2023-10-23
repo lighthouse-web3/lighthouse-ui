783c2d2f36bc50c6bd002e7ffc642072d95fa1e2
arpit <arpit@lighthouse.storage>
2023-10-23 12:57:43 +0530

    GTAG Fix

--- components/metadata/Metadata.jsx
+++ components/metadata/Metadata.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import Script from "next/script";
 
 function Metadata({
   title = "Lighthouse",
@@ -11,41 +12,55 @@ function Metadata({
   const router = useRouter();
 
   return (
-    <Head>
-      <title>Lighthouse Perpetual Storage</title>
-      <meta name="title" content="Lighthouse Perpetual Storage" />
-      <meta
-        name="description"
-        content="Store Files Perpetually on IPFS and Filecoin"
-      />
+    <>
+      <Head>
+        <title>Lighthouse Perpetual Storage</title>
+        <meta name="title" content="Lighthouse Perpetual Storage" />
+        <meta
+          name="description"
+          content="Store Files Perpetually on IPFS and Filecoin"
+        />
 
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.lighthouse.storage/" />
-      <meta property="og:title" content="Lighthouse Perpetual Storage" />
-      <meta
-        property="og:description"
-        content="Store Files Perpetually on IPFS and Filecoin"
-      />
-      <meta
-        property="og:image"
-        content="https://gateway.lighthouse.storage/ipfs/Qmd7rR9EPKomhmoRUw2WB7FJAeSWAtC8c1nkKgGZL39LpB"
-      />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.lighthouse.storage/" />
+        <meta property="og:title" content="Lighthouse Perpetual Storage" />
+        <meta
+          property="og:description"
+          content="Store Files Perpetually on IPFS and Filecoin"
+        />
+        <meta
+          property="og:image"
+          content="https://gateway.lighthouse.storage/ipfs/Qmd7rR9EPKomhmoRUw2WB7FJAeSWAtC8c1nkKgGZL39LpB"
+        />
 
-      {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.lighthouse.storage/" />
-      <meta property="twitter:title" content="Lighthouse Perpetual Storage" />
-      <meta name="msvalidate.01" content="604856DE8115C3F615EDBDB485DD021A" />
-      <meta
-        property="twitter:description"
-        content="Store Files Perpetually on IPFS and Filecoin"
-      />
-      <meta
-        property="twitter:image"
-        content="https://gateway.lighthouse.storage/ipfs/Qmd7rR9EPKomhmoRUw2WB7FJAeSWAtC8c1nkKgGZL39LpB"
-      />
-    </Head>
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.lighthouse.storage/"
+        />
+        <meta property="twitter:title" content="Lighthouse Perpetual Storage" />
+        <meta name="msvalidate.01" content="604856DE8115C3F615EDBDB485DD021A" />
+        <meta
+          property="twitter:description"
+          content="Store Files Perpetually on IPFS and Filecoin"
+        />
+        <meta
+          property="twitter:image"
+          content="https://gateway.lighthouse.storage/ipfs/Qmd7rR9EPKomhmoRUw2WB7FJAeSWAtC8c1nkKgGZL39LpB"
+        />
+      </Head>
+
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-8NEX7J7T7K"></Script>
+      <Script id="google-analytics">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8NEX7J7T7K');`}
+      </Script>
+    </>
   );
 }
 
